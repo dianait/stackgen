c18e81bc2ff225fd61722f25f831dc025e7b3ae9
Diana <dianahdezsoler@gmail.com>
2020-08-09 11:41:06 +0200

    useStack custom hook added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/header";
 import Stack from "./components/stack";
-import { defaultStackComplete } from "./hooks/useStack";
-
-/* import { useStack } from "./hooks/useStack"; */
+import { useStack } from "./hooks/useStack";
 
 export default function App() {
   const [keyword, setKeyword] = useState([]);
-  const [stack, setStack] = useState(["angular.png", "js.png"]);
 
-  /*   const { stack } = useStack({ keyword }); */
-
-  useEffect(() => {
-    const url = `${keyword}.png`;
-    const defaultStack = defaultStackComplete();
-
-    // Si tenemos imagen y si no está ya en eñ stack del usuario
-    if (defaultStack.includes(url) && !stack.includes(url)) {
-      stack[stack.length] = url;
-      console.log(`stack updated con ${keyword}`, stack);
-      setStack(stack);
-    }
-  }, [stack, keyword]);
+  const { stack } = useStack({ keyword });
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
--- src/components/form.js
+++ src/components/form.js
@@ -3,43 +3,23 @@ import updateStack from "../services/udpdateStack";
 
 export default function Form() {
   return (
-    <div style={{ backgroundColor: "#282c34" }}>
-      <datalist id="stackList">
-        <option value="chrome" />
-        <option value="html" />
-        <option value="css" />
-        <option value="notion" />
-        <option value="apple" />
-        <option value="dolce" />
-        <option value="gitkraken" />
-        <option value="angular" />
-        <option value="next" />
-        <option value="linux" />
-        <option value="bose" />
-        <option value="msi" />
-        <option value="honor" />
-        <option value="ts" />
-        <option value="js" />
-        <option value="vsc" />
-      </datalist>
-      <form>
-        <input
-          type="serch"
-          list="stackList"
-          placeholder="🔎 Buscar..."
-          onKeyPress={(event) => {
-            if (event.key === "Enter") {
-              const url = `${event.target.value}.png`;
-              updateStack(url);
-            }
-          }}
-        />
-      </form>
+    <form>
+      <input
+        type="serch"
+        list="stackList"
+        placeholder="🔎 Buscar..."
+        onKeyPress={(event) => {
+          if (event.key === "Enter") {
+            const url = `${event.target.value}.png`;
+            updateStack(url);
+          }
+        }}
+      />
       <br />
       <br />
       <br />
       <br />
-    </div>
+    </form>
   );
 }
 
