b41e6c7df5727957d698da895c5b7d12db198354
Diana <dianahdezsoler@gmail.com>
2020-08-10 19:12:50 +0200

    form extract as component

--- src/App.js
+++ src/App.js
@@ -5,67 +5,32 @@ import Stack from "./components/stack";
 import { useStack } from "./hooks/useStack";
 import html2canvas from "html2canvas";
 import Modal from "./components/modal";
+import Form from "./components/form";
 
 export default function App() {
   // STATES
-  const [keyword, setKeyword] = useState(null);
-  const [message, setMessage] = useState(null);
   const [modal, setModal] = useState(false);
-
-  // HOOKS
-  const { stack, msg } = useStack({ keyword });
-
-  // REFS
-  const inputRef = useRef();
-
-  // EFFECTS
-  useEffect(() => {
-    inputRef.current.value = "";
-  }, [stack]);
-
-  // HANDLES
-  const handleChange = (evt) => {
-    setMessage("");
-    inputRef.current.value = evt.target.value;
-    setKeyword(evt.target.value);
-  };
-
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    setMessage(msg);
-    inputRef.current.value = "";
-    console.log(msg);
-  };
+  const [stack, setStack] = useState([]);
 
   const handleOnClick = (evt) => {
-    const show = modal === "block" ? "none" : "block";
-    setModal(show);
+    setModal("block");
 
     html2canvas(document.querySelector(".stack")).then((canvas) => {
-      const m = document.getElementsByClassName("modal")[0];
-      m.append(canvas);
+      const modalElement = document.getElementsByClassName("modal")[0];
+      modalElement.append(canvas);
     });
+  };
+
+  const onSubmit = (stack) => {
+    setStack(stack);
   };
 
   return (
     <div className="App">
       <Header title="stackGen" />
       <Modal show={modal} />
-      <form onSubmit={handleSubmit} style={{ backgroundColor: "#282c34" }}>
-        <input
-          ref={inputRef}
-          type="text"
-          name="keyword"
-          placeholder="🔎 Buscar..."
-          onChange={handleChange}
-          autoComplete="off"
-        />
-        <p className="msg">{message}</p>
-        <br />
-        <br />
-      </form>
+      <Form onSubmit={onSubmit} />
       <button onClick={handleOnClick}>Generate!</button>
-
       <Stack className="stack" stack={stack} />
     </div>
   );
--- src/components/form.js
+++ src/components/form.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { useStack } from "../hooks/useStack";
 
-export default function Form() {
+export default function Form({ onSubmit: handlerSumitFromParent }) {
+  // STATES
+  const [keyword, setKeyword] = useState(null);
+  const [message, setMessage] = useState(null);
+
+  // REFS
+  const inputRef = useRef();
+
+  // HOOKS
+  const { stack, msg } = useStack({
+    keyword,
+  });
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    setKeyword(evt.target.value);
+    setMessage(msg);
+    inputRef.current.value = "";
+    handlerSumitFromParent(stack);
+  };
+
+  // HANDLES
+  const handleChange = (evt) => {
+    setMessage("");
+    inputRef.current.value = evt.target.value;
+    setKeyword(evt.target.value);
+  };
+
   return (
-    <form onSubmit={handleSubmit} style={{ backgroundColor: "#282c34" }}>
+    <form
+      onSubmit={handleSubmit}
+      style={{
+        backgroundColor: "#282c34",
+      }}
+    >
       <input
         ref={inputRef}
         type="text"
