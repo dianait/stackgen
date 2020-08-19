41d644270487fb7c14cf3aab2d814a183678c861
Diana <dianahdezsoler@gmail.com>
2020-08-19 20:41:57 +0200

    more icons added

--- src/data/index.js
+++ src/data/index.js
@@ -1,25 +1,3 @@
-export default function defaultStackComplete() {
-  return [
-    "apple.png",
-    "bose.png",
-    "chrome.png",
-    "css.png",
-    "gitkraken.png",
-    "dolce.png",
-    "html.png",
-    "msi.png",
-    "linux.png",
-    "notion.png",
-    "vsc.png",
-    "js.png",
-    "ts.png",
-    "next.png",
-    "honor.png",
-    "node.png",
-    "angular.png",
-  ]
-}
-
 export const defaultStackSVG = [
   "apple.svg",
   "bose.svg",
